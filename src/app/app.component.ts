5634c5526897f52924167d3e250654625b012849
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-30 22:03:15 -0500

    feat: add route, add material, add i18n, add envs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'learning-center';
+
+  constructor(private translateService: TranslateService) {
+    translateService.use('en');
+    translateService.addLangs(['en', 'es']);
+
+    const env = environment.production;
+    console.log(env);
+  }
+
 }
