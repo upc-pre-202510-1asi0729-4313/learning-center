363b5e63be22bc35809de3ecd5305e4728f88fd1
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-04-30 22:37:25 -0500

    feat: add route paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
